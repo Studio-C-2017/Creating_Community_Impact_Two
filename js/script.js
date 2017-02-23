4494057abcc0a7d39a61849bcbaf7c36cf1abfdf
kenstjonweeks <kennethweeks@Kenneths-MacBook-Pro.local>
2017-02-23 10:07:39 -0800

    Here are some more changes that need to be fixed
    hello

--- js/script.js
+++ js/script.js
@@ -21,29 +21,33 @@ const rightArrow = document.getElementById('right-click');
 const videoSel = document.getElementsByClassName('vid-sel');
 
 var j = -1;  // first click 0, second click 1;
-var a = 0; // first click 1, second click 2;
 
 function rightArrowClick() {
 
-	j++;
-	a++;
-	videoSel[j].style.right = "100%";
-	videoSel[a].style.right = "0%";
+	if (j < 1) {	
+		j++;
+		videoSel[j].style.marginLeft = "-105%";
+	} else {
+		j = -2;
+		videoSel[0].style.marginLeft = "0%";
+	}
 }
 
 rightArrow.addEventListener('click', function() {
+
+	alert(j);
 
 	rightArrowClick();
 
 });
 
+/*b = j -1;
+c = a;
+
 function leftArrowClick() {
 
-	j--;
-	a--;
-
-	videoSel[j].style.right = "0%";
-	videoSel[a].style.right = "-100%";
+	videoSel[b].style.marginLeft = "0%";
+	videoSel[c].style.marginLeft = "-100%";
 }
 
 
@@ -51,7 +55,7 @@ leftArrow.addEventListener('click', function() {
 
 	leftArrowClick();
 
-});
+});*/
 /*function leftArrowClick() {
 	var a = j--;
 	foodpopdis[a].style.marginLeft = "3.5%";
